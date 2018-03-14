d4ec8d6bc9a1a9eb37af91f615b5d9d83bf63a2e
Syd <slynch2@uoregon.edu>
2018-03-13 21:33:34 -0700

    svm example

--- digit/digit_reader.py
+++ digit/digit_reader.py
@@ -23,17 +23,19 @@ def parse_data(file_name):
 def main(argv):
     variables, rows = parse_data(argv[0])
 
+    # from kernel https://www.kaggle.com/archaeocharlie/a-beginner-s-approach-to-classification
     labeled_images = pd.read_csv(argv[0])
     images = labeled_images.iloc[0:5000,1:]
     labels = labeled_images.iloc[0:5000,:1]
     train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
-    i=1
-    img=train_images.iloc[i].as_matrix()
-    img=img.reshape((28,28))
-    plt.imshow(img,cmap='gray')
-    plt.title(train_labels.iloc[i,0])
-    plt.show()
+    
 
+    test_images[test_images>0]=1
+    train_images[train_images>0]=1
+
+    clf = svm.SVC()
+    clf.fit(train_images, train_labels.values.ravel())
+    print(clf.score(test_images,test_labels))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
