411af1bd19acceb75ca0ae06a37a1df07c958d56
Syd <slynch2@uoregon.edu>
2018-03-13 22:38:34 -0700

    tune parameters

--- digit/digit_reader.py
+++ digit/digit_reader.py
@@ -23,19 +23,23 @@ def parse_data(file_name):
 def main(argv):
     variables, rows = parse_data(argv[0])
 
+    # svm
     # from kernel https://www.kaggle.com/archaeocharlie/a-beginner-s-approach-to-classification
     labeled_images = pd.read_csv(argv[0])
     images = labeled_images.iloc[0:5000,1:]
     labels = labeled_images.iloc[0:5000,:1]
     train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
-    
+
 
     test_images[test_images>0]=1
     train_images[train_images>0]=1
 
-    clf = svm.SVC()
+    clf = svm.SVC(C=7, kernel='rbf', gamma=0.009)
     clf.fit(train_images, train_labels.values.ravel())
     print(clf.score(test_images,test_labels))
+
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
