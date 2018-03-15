0b55a0d9c166dd22ccf61a053b14e9128c618548
Syd <slynch2@uoregon.edu>
2018-03-15 16:03:53 -0700

    svm

--- digit/digit_reader.py
+++ digit/digit_reader.py
@@ -19,20 +19,12 @@ def parse_data(file_name):
     return (variables, rows)
 
 
-
-
-
-
-
-def main(argv):
-    training_data = argv[0]
-    test_data = argv[1]
-
+def support_vector_machine_train(training_data):
     '''
     svm
     from kernel https://www.kaggle.com/archaeocharlie/a-beginner-s-approach-to-classification
     '''
-    labeled_images = pd.read_csv(argv[0])
+    labeled_images = pd.read_csv(training_data)
     images = labeled_images.iloc[0:10000,1:]
     labels = labeled_images.iloc[0:10000,:1]
     train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
@@ -41,11 +33,22 @@ def main(argv):
     test_images[test_images>0]=1
     train_images[train_images>0]=1
 
+    # can tune these parameters
+    # see http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
     clf = svm.SVC(C=7, kernel='rbf', gamma=0.009)
     clf.fit(train_images, train_labels.values.ravel())
-    print(clf.score(test_images,test_labels))
+    print("Accuracy: ", clf.score(test_images,test_labels))
+
+    return clf
+
+def main(argv):
+    training_data = argv[0]
+    test_data = argv[1]
+
+
 
     # test svm
+    clf = support_vector_machine_train(training_data)
     test = pd.read_csv(test_data)
     test[test>0]=1
     results = clf.predict(test[0:])
@@ -55,6 +58,7 @@ def main(argv):
     df.index.name = 'ImageId'
     df.columns=['Label']
     df.to_csv('results.csv', header=True)
+
 
 
 if __name__ == "__main__":
